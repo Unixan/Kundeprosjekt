a8c2d141694178962314ba1cc522b95fcd68f0e0
Unixan <115973884+Unixan@users.noreply.github.com>
2023-01-23 10:45:53 +0100

    Modell start

--- JS/model.js
+++ JS/model.js
@@ -1,3 +1,41 @@
 model = {
+  //user
   state: "mainView",
+
+  //input
+  inputs: {
+    user: {
+      search: "",
+      pictureComment: "",
+      userEmail: "",
+      filter: [
+        { name: "Fargerik", checked: false },
+        { name: "Mørk", checked: false },
+        { name: "Svart/Hvit", checked: false },
+        { name: "Arkitektur", checked: false },
+        { name: "By", checked: false },
+        { name: "Natur", checked: false },
+      ],
+    },
+    admin: {},
+  },
+  //data
+  pictures: [
+    {
+      img: "",
+      id: "",
+      title: "",
+      description: "",
+      artist: "",
+      category: [],
+      comments: [{ user: "", comment: "" }],
+    },
+  ],
+  SoMelinks: [
+    { media: "Facebook", logoimg: "", link: "" },
+    { media: "SnapChat", logoimg: "", link: "" },
+    { media: "Twitter", logoimg: "", link: "" },
+    { media: "Instagram", logoimg: "", link: "" },
+  ],
+  email: "fakeEmail@mail.com",
 };
