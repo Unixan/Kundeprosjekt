8c0b43b448bdfc9a67c696eaa17693d5ff6fcbd8
Nikolaipe <nikolaipe@getacademy.no>
2023-01-26 10:55:07 +0100

    addImageView expleined
    skrev mer utdypende hva jeg vil gjøre i filen

--- JS/ADMIN/addImageView.js
+++ JS/ADMIN/addImageView.js
@@ -1,3 +1,14 @@
 // addImageView skal brukes til å legge til bilder fra adminView når en trykker "+". Skal også kunne brukes til å redigere eksisterende bilder
 //TODO everything. What you mean every- EEEEVEEERYYYTHIIING!
 //ON THE CASE: Nikotron
+
+/*TODO:
+    skrive view for å kunne legge til:
+         nytt bilde
+         bildetittel
+         beskrivelse av bilde
+         hvilke tags bildet skal ha
+         nye tags
+    funksjonen trenger argument for å kunne velge allerede eksisterende bilde for redigering
+    bildet skal legges til i model.pictures, eller endre model.pictures[argument]
+    */
