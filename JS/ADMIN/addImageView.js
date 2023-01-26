f57a05219bd74b3953651f5dc46a8e25c8180243
Nikolaipe <nikolaipe@getacademy.no>
2023-01-26 13:53:29 +0100

    started addImageView
    Ikke ferdig med addpicview, men starta

--- JS/ADMIN/addImageView.js
+++ JS/ADMIN/addImageView.js
@@ -12,3 +12,20 @@
     funksjonen trenger argument for å kunne velge allerede eksisterende bilde for redigering
     bildet skal legges til i model.pictures, eller endre model.pictures[argument]
     */
+   function updateAddImageView(index){
+    //skriver skjermbilde for endring eller redigering av bilder
+    let html = "";
+    if(!index){
+        //om det ikke er noen index, så lager vi nytt bilde med tomme verdier fra model.inputs
+    model.inputs.admin.category = model.filter;
+    html = /*HTML*/ `
+    <h1>Legg til nytt bilde</h1>
+    <div>
+        <input type="text" placeholder="Skriv inn bildetittel" onchange="model.inputs.admin.addPic.title = this.value">
+        <div> //TODO: insert fancy schmancy bildeopplastning here </div>
+        <input type="text" placeholder="Skriv inn beskrivelse" onchange="model.inputs.admin.addPic.description = this.value">
+    </div>
+    `;
+   }
+    }
+}
