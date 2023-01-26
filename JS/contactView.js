e510792d498790b1a5063379363ea773319c0f6d
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-01-26 14:28:24 +0100

    Lagt tilbakeknapp oppe og med pil

--- JS/contactView.js
+++ JS/contactView.js
@@ -8,17 +8,17 @@
 /*DOES IT: Erwan Foxtail*/
 function updateContactView(){
     document.getElementById('app').innerHTML = /*html*/ `
+    <div><a onclick="backButton()"><img src=${model.backLogo}></a></div>
     <h1>${model.creator.name}</h1>
     <p>${model.creator.title}</p>
     <p><a>kontakt</a>
     <div>
     <img src=${model.creator.aboutPicture} alt="Profilbilde av Jarand Midtgaard">
     <div> <!--SoMe linker-->
-        <div><a><img src=${model.creator.creatorSoMelinks[0].logoimg}></a></div>
+    <div><a><img src=${model.creator.creatorSoMelinks[0].logoimg}></a></div>
         <div><a><img src=${model.creator.creatorSoMelinks[1].logoimg}></a></div>
         <div><a><img src=${model.creator.creatorSoMelinks[2].logoimg}></a></div>
         <div><a><img src=${model.creator.creatorSoMelinks[3].logoimg}></a></div>
     </div>
-    <button onclick="mainViewState()">Tilbake</button>
     `
 }
