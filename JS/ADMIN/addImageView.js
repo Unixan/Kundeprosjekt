f3075a15e454199f4c3e9c8b6a072dfc8f7f1248
Nikolaipe <nikolaipe@getacademy.no>
2023-01-26 14:46:25 +0100

    Can select categories when adding new images
    checkboxer endrer verdi i model.inputs

--- JS/ADMIN/addImageView.js
+++ JS/ADMIN/addImageView.js
@@ -15,15 +15,34 @@
    function updateAddImageView(index){
     //skriver skjermbilde for endring eller redigering av bilder
     let html = "";
-    if(!index){
-        //om det ikke er noen index, så lager vi nytt bilde med tomme verdier fra model.inputs
+    if(!index){//om det ikke er noen index, så lager vi nytt bilde med tomme verdier fra model.inputs
     model.inputs.admin.category = model.filter;
+    let catecoryDiv = "";
+    for (let i = 0; i < model.inputs.admin.category.length; i++){//genererer categoribokser
+        catecoryDiv += /*HTML*/ `
+        <div> 
+            <input 
+                type="checkbox" 
+                name="${model.inputs.admin.category[i].cat}"
+                ${model.inputs.admin.category ? "" : "checked"} 
+                onchange="model.inputs.admin.category[${i}].checked = !model.inputs.admin.category[${i}].checked"
+            >
+            <label for="${model.inputs.admin.category[i].cat}">
+            ${model.inputs.admin.category[i].cat}
+            </label> 
+            <br>
+        `;
+    }
     html = /*HTML*/ `
     <h1>Legg til nytt bilde</h1>
     <div>
         <input type="text" placeholder="Skriv inn bildetittel" onchange="model.inputs.admin.addPic.title = this.value">
         <div> //TODO: insert fancy schmancy bildeopplastning here </div>
         <input type="text" placeholder="Skriv inn beskrivelse" onchange="model.inputs.admin.addPic.description = this.value">
+        <div>
+            ${catecoryDiv}
+            //TODO fancy schmancy måte å legge til nye kategorier som jeg ikke har tid til nå
+        </div>
     </div>
     `;
 }
