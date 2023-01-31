98aa24f9a6f8af9177b2be84507aa2ab0ed1415e
Unixan <115973884+Unixan@users.noreply.github.com>
2023-01-31 12:35:46 +0100

    Setter opp mobil kompatibilitet

--- JS/mainController.js
+++ JS/mainController.js
@@ -1,7 +1,2 @@
 //controllerfunksjonene til mainView
 
-function showPicture(index){
-    //ser om bildet er trykket på eller ikke, så endrer til motsatt.
-    model.pictures[index].toggled = !model.pictures[index].toggled;
-    updateView();
-}
