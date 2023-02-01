4ed83e6c4efe8cf2320a3d81e3db1fa2152d8d2c
RebeckaKinn <rebeckaki@getacademy.no>
2023-02-01 13:39:02 +0100

    login css rework AGAIN

--- JS/ADMIN/loginView.js
+++ JS/ADMIN/loginView.js
@@ -8,33 +8,34 @@ NEED
 function updateLoginView(){
     appDiv.innerHTML = /*HTML*/`
     <div class="logIn-Field">
-        <div class="logIn-Title">Logg-inn</div>
-        <label class="logIn-UsernameTitle">Brukernavn</label>
+        <div class="logIn-Title">Administrerende logg-inn</div>
+        <label class="logIn-UnderTitle">Brukernavn</label>
         <input type="text" 
             id="usernameInput"
             name="usernameInput"
-            class="logIn-InputUsername"
+            class="logIn-Input"
             placeholder="Skriv inn brukernavn" 
             autocomplete="off"
             oninput="model.inputs.login.username=this.value" 
             value='${model.inputs.login.username}'/>
         <b id="wrongUsername" class="wrongUsername">Må ha et gyldig brukernavn.</b>
 
-        <label class="logIn-PasswordTitle">Passord</label>
+        <label class="logIn-UnderTitle">Passord</label>
         <input type="password" 
             id="passwordInput"
             name="passwordInput"
-            class="logIn-InputPassword"
+            class="logIn-Input"
             placeholder="Skriv inn passord" 
             autocomplete="off"
             oninput="model.inputs.login.password=this.value"
             value='${model.inputs.login.password}' 
             onkeydown="if(event.code === 'Enter') login()"/>
             <i class="passwordEye" onclick="togglePassword()"><img src="IMG/ICONS/password_eye.png"></i>
-        <b id="wrongPassword" class="wrongPassword">Må ha et gyldig passord</b>
+        <b id="wrongPassword" class="wrongUsername">Må ha et gyldig passord</b>
 
         <button type="submit" class="logIn-button" onclick="login()">Logg inn</button>
     </div>
     `;
+
 }
 
