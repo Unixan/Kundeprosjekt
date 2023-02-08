9f2c055cf49880b53ac5a24d05054d509154fce2
Nikolaipe <nikolaipe@getacademy.no>
2023-02-08 11:17:22 +0100

    Fårr til getProjects()
    returnerer et array med objekter som det går an å jobbe med

--- JS/common.js
+++ JS/common.js
@@ -2,17 +2,28 @@ function getProjects() {
     //TODO make this shit work
   //tråler gjennom model.pictures[index].projectNumber og .projectName
   //returnerer array med {projectName:"(...)", projectNumber:(...)}
-  const projectArray = [];
+  const numbers = [];
+  const names = [];
   let picture = model.pictures;
   for (let i = 0; i < picture.length; i++) {
-      
+      if(numbers.includes(picture[i].projectNumber)){
+          continue;
+      } else numbers.push(picture[i].projectNumber);
+      if(names.includes(picture[i].projectName)){
+          continue;
+      } else names.push(picture[i].projectName);
     }
-    console.log(projectArray, "liste med objekter av prosjekter");
+    return makeProjectArray(names, numbers);
 }
-//   const numbers = [];
-    //     if(numbers.includes(picture[i].projectId.number)){
-    //         console.log("trigg");
-    //         continue;
-    //     } else numbers.push(picture[i].projectId.number);
-    //   }
-    //   console.log(numbers, "liste over prosjektnummer");
+
+function makeProjectArray(name, number){
+    const projectArray = [];
+    for (let i = 0; i < name.length; i++){
+        const projectObject = {
+            projectName: name[i],
+            projectNumber: number[i]
+        }
+        projectArray.push(projectObject);
+    }
+    return projectArray;
+}
