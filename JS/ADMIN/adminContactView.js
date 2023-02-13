6a9f8f510fd9928666de491db28488ffd0b0c775
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-02-13 10:43:30 +0100

    lagt til css collabinfo-box

--- JS/ADMIN/adminContactView.js
+++ JS/ADMIN/adminContactView.js
@@ -77,7 +77,7 @@ function updateAdminContactView() {
 
   document.getElementById('app').innerHTML = /*html*/ `
     ${menuBar()}
-    
+  <div class="collabInfo-box">
     <h1>${model.creator.name}</h1>
     <p>${model.creator.title}</p>
     <p><a href="mailto:${model.creator.email}">kontakt</a>
